c004418a0920d0cb2215b89f5aa51ce40789d4e8
antonia <antonia.mab@gmail.com>
2019-04-07 19:05:44 +0100

    Solutions for Fibonnaci (with MEMOIZATION) and Data Structure Queue

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,65 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+// Using ITERATION
+// Runtime Complexity: Linear Runtime
+// function fib(n) {
+//     var fibArr = [0, 1];
+//
+//     for(let i = 2; i <= n; i++) {
+//         fibArr[i] = fibArr[i-1] + fibArr[i-2];
+//     }
+//
+//     return fibArr[n];
+// }
+
+// Using RECURSION
+// Runtime Complexity: Exponential Runtime
+// function fib (n) {
+//     if (n < 2) {
+//         return n;
+//     }
+//
+//     return fib(n-1) + fib(n-2);
+// };
+
+// Using RECURSION with improved performance (MEMOIZATION)
+// To avoid calling the function with the same argument
+// multiple times, we can store the arguments of each
+// function call along with the result.
+function memoize (fn) {
+    const cache = {};
+
+    return function (...args) {
+
+        // has this function ever been called
+        // with this particular part of args before?
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        // call the function with the given arguments
+        const result = fn.apply(this, args);
+
+        // and store in cache
+        cache[args] = result;
+
+        return result;
+    };
+};
+
+function slowFib (n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n-1) + fib(n-2);
+};
+
+// reassign "fib" to be the memoized version
+const fib = memoize(slowFib);
+// fib = function (...args) { .... };
+// fib(4) -> function (4) { .... };
 
 module.exports = fib;
